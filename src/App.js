c8a296dde0da30e4f60d7ac499717127d3633d77
dorrrn <doan7tran@gmail.com>
2022-02-22 21:21:38 +0100

    done

--- src/App.js
+++ src/App.js
@@ -16,49 +16,69 @@ function App() {
     const sortedContacts = contactsArr.sort((a, b) => {
       return a.name.localeCompare(b.name);
     });
-    setContacts([...sortedContacts]);
+    setContacts(sortedContacts);
   };
 
   const sortByPop = () => {
     const sortedContacts = contactsArr.sort((a, b) => {
       return b.popularity - a.popularity;
     });
-    setContacts([...sortedContacts]);
+    setContacts(sortedContacts);
+  };
+
+  const removeContact = (id) => {
+    const filteredContacts = contactsArr.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(filteredContacts);
   };
 
   return (
     <div className="App">
-      <h1>Iron Contacts</h1>
-      <button onClick={addRandomContact}>Add random contact</button>
-      <button onClick={sortByName}>Sort by name</button>
-      <button onClick={sortByPop}>Sort by popularity</button>
+      <div className="navbar">
+        <h1>Movie stars</h1>
+      </div>
 
-      <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won an oscar</th>
-          <th>Won an emmy</th>
-        </tr>
-        {contacts.map((contact) => {
-          return (
-            <tr>
-              <td>
-                <img
-                  src={contact.pictureUrl}
-                  alt={contact.name}
-                  className="img-contact"
-                />
-              </td>
-              <td>{contact.name}</td>
-              <td>{Math.round(contact.popularity * 100) / 100}</td>
-              <td>{contact.wonOscar && <p>🏆</p>} </td>
-              <td>{contact.wonEmmy && <p>🏆</p>} </td>
-            </tr>
-          );
-        })}
-      </table>
+      <div className="main-content">
+        <div className="btn-bar">
+          <button onClick={addRandomContact}>Add random contact</button>
+          <button onClick={sortByName}>Sort by name</button>
+          <button onClick={sortByPop}>Sort by popularity</button>
+        </div>
+        <table className="center-table">
+          <tr className="table-head">
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an oscar</th>
+            <th>Won an emmy</th>
+            <th>Actions</th>
+          </tr>
+
+          {contacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt={contact.name}
+                    className="img-contact"
+                  />
+                </td>
+                <td>{contact.name}</td>
+                <td>{Math.round(contact.popularity * 100) / 100}</td>
+                <td>{contact.wonOscar && <p>🏆</p>} </td>
+                <td>{contact.wonEmmy && <p>🎖️</p>} </td>
+                <td>
+                  <button onClick={() => removeContact(contact.id)}>
+                    Remove contact
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 }
