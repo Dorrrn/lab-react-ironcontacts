f5327e3d1865ca598ede1ccd52dc739ab1b97650
dorrrn <doan7tran@gmail.com>
2022-02-22 19:43:07 +0100

    Iteration 1,2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import { useState } from "react";
 import "./App.css";
 import contactsArr from "./contacts.json";
-import Contacts from "./components/Contacts"
 
 function App() {
-  const [contacts, setContacts] = useState(contactsArr.slice(0,5));
+  let fiveContacts = contactsArr.slice(0, 5);
+  const [contacts, setContacts] = useState(fiveContacts);
 
   return (
-  <div className="App">
-
-<Contacts 
-  name = {contacts.name}
-  populartiy = {contacts.popularity}
-  image = {contacts.pictureUrl}
-/>
-  </div>
+    <div className="App">
+      <h1>Iron Contacts</h1>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an oscar</th>
+          <th>Won an emmy</th>
+        </tr>
+        {contacts.map((contact) => {
+          return (
+            <tr>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                  className="img-contact"
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar && <p>🏆</p>} </td>
+              <td>{contact.wonEmmy && <p>🏆</p>} </td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
   );
 }
+
 export default App;
