fbcc8131d68ac4625350f7028122674c10451195
dorrrn <doan7tran@gmail.com>
2022-02-22 18:31:32 +0100

    added Contacts component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsArr from "./contacts.json";
+import Contacts from "./components/Contacts"
 
 function App() {
+  const [contacts, setContacts] = useState(contactsArr.slice(0,5));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+
+<Contacts 
+  name = {contacts.name}
+  populartiy = {contacts.popularity}
+  image = {contacts.pictureUrl}
+/>
+  </div>
   );
 }
-
 export default App;
